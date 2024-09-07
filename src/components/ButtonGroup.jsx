6b02f705ee11f8fa2e1e938bfbd3271045fa41d2
Shaker <133087050+IliyaZafirov@users.noreply.github.com>
2024-09-07 20:44:49 +0300

    added props into secondary buttons group

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,18 +1,17 @@
 import Button from "./Button";
 
+const secondaryButtons = [
+  'Mark all as complete',
+  'Mark all as incomplete',
+  'Reset to initial',
+  'Remove all items'
+];
+
 export default function ButtonGroup() {
-
-  const secondaryButtons = [
-    'Mark all as complete',
-    'Mark all as incomplete',
-    'Reset to initial',
-    'Remove all items'
-  ];
-
   return (
     <section className="button-group">
       {
-        secondaryButtons.map(btn => { return <Button type='secondary'>{btn}</Button> })
+        secondaryButtons.map((text, idx) => { return <Button key={text} type='secondary'>{text}</Button> })
       }
     </section>
   )
